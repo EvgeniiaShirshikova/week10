8f1c9fdeadeb7f5f532b82ef44706987fed66587
EvgeniiaShirshikova <shirshikovae@gmail.com>
2023-02-13 12:09:03 -0600

    js

--- index.js
+++ index.js
@@ -7,3 +7,23 @@ function showMessage()
 alert('Hello everyone!');
 }
 showMessage();
+
+function sum(a, b) {
+    return a + b;
+}
+
+//alert(sum(5,6));
+
+function y(x) {
+    return x**2;
+}
+
+let result = y(2);
+//alert(result);
+
+function showMessageFromUser(name, message) {
+    //alert(name+': '+message);
+    alert(`${name}: ${message}`);
+}
+
+showMessageFromUser('Jane', 'hello');
