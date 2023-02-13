d8bc7a6bd1ccd4a5e5473b89b9f662f47ea4cbd4
EvgeniiaShirshikova <shirshikovae@gmail.com>
2023-02-13 11:58:30 -0600

    js

---
+++ index.js
@@ -0,0 +1,9 @@
+//let age = prompt('How old are you?');
+//alert(`You are ${age}!`);
+//let yes = confirm('Are we continuing to talk?');
+//alert('You chose ' + yes);
+function showMessage()
+{
+alert('Hello everyone!');
+}
+showMessage();
