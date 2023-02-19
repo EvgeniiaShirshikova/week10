6e4d1e664a4efdedca0a2a5693a37e5373d74b61
EvgeniiaShirshikova <shirshikovae@gmail.com>
2023-02-18 21:13:46 -0600

    js

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ console.log('Я учу Javascript!');
 
 let cat = document.getElementById("cat");
 
-function showCat1() {
-cat.src = "./assets/pictures/cat1.png"
-};
+let showCat1 = () => cat.src = "./assets/pictures/cat1.png";
 
-function showCat2() {
-    cat.src = "./assets/pictures/cat2.jpg"
-};
+let showCat2 = () => cat.src = "./assets/pictures/cat2.jpg";
