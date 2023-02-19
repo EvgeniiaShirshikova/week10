095799f733713df114272c203146fe347b5429c1
EvgeniiaShirshikova <shirshikovae@gmail.com>
2023-02-18 20:47:21 -0600

    js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 console.log('Я учу Javascript!');
+
+let cat1 = document.getElementById("cat1");
+let cat2 = document.getElementById("cat2");
+
+function showCat1() {
+cat1.style.display = "flex"
+cat2.style.display = "none"
+}
+
+function showCat2() {
+    cat1.style.display = "none"
+    cat2.style.display = "flex"
+}
